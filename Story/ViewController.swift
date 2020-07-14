2e98b1e6147e292578901ffedec49bcb0874435b
vo14ek <serj.petkoglo@gmail.com>
2020-07-14 17:08:36 +0300

    Add func

--- Story/ViewController.swift
+++ Story/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
 
 
 }
